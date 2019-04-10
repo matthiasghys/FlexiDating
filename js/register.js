09dc92b40535c2236454628fd91c8cb980acc7c1
Christophe Peeters <peeters1993@msn.com>
2019-04-10 15:29:07 +0200

    Validation 1.0 register

--- js/register.js
+++ js/register.js
@@ -1,62 +1,59 @@
-    document.querySelector('#registerBtn').addEventListener('click', function(e) {
-        if(document.getElementById('form').checkValidity()){
-            let url = 'https://scrumserver.tenobe.org/scrum/api/profiel/create.php';
+document.querySelector('#registerBtn').addEventListener('click', function (e) {
+    if (document.getElementById("form").checkValidity()){
+    e.preventDefault();
 
-            const dag = document.getElementById('inputDateDay').value;
-            const maand = document.getElementById('inputDateMonth').value;
-            const year = document.getElementById('inputDateYear').value;
+    let url = 'https://scrumserver.tenobe.org/scrum/api/profiel/create.php';
 
-            const datum = year + "-" + maand + "-" + dag;
-            let foto = "";
-            if(document.getElementById('inputGender').value === "m"){
-                foto = 'https://scrumserver.tenobe.org/scrum/img/man_7.png'
-            }else{
-                foto = 'https://scrumserver.tenobe.org/scrum/img/woman_8.png'
-            }
+    const dag = document.getElementById('inputDateDay').value;
+    const maand = document.getElementById('inputDateMonth').value;
+    const year = document.getElementById('inputDateYear').value;
 
-            let data = {
-                familienaam: document.getElementById('inputLastName').value,
-                voornaam: document.getElementById('inputFirstName').value,
-                geboortedatum: datum,
-                email: document.getElementById('inputEmail').value,
-                nickname: document.getElementById('inputUsername').value,
-                foto: foto,
-                beroep: document.getElementById('inputProfession').value,
-                sexe: document.getElementById('inputGender').value,
-                haarkleur: document.getElementById('inputHairColor').value,
-                oogkleur: document.getElementById('inputEyeColor').value,
-                grootte: document.getElementById('inputHeight').value,
-                gewicht: document.getElementById('inputWeight').value,
-                wachtwoord: document.getElementById('inputPassword1').value,
-                lovecoins: 3
+    const datum = year + "-" + maand + "-" + dag;
+    let foto = "";
+    if (document.getElementById('inputGender').value === "m") {
+        foto = 'https://scrumserver.tenobe.org/scrum/img/man_7.png'
+    } else {
+        foto = 'https://scrumserver.tenobe.org/scrum/img/woman_8.png'
+    }
 
-            };
+    let data = {
+        familienaam: document.getElementById('inputLastName').value,
+        voornaam: document.getElementById('inputFirstName').value,
+        geboortedatum: datum,
+        email: document.getElementById('inputEmail').value,
+        nickname: document.getElementById('inputUsername').value,
+        foto: foto,
+        beroep: document.getElementById('inputProfession').value,
+        sexe: document.getElementById('inputGender').value,
+        haarkleur: document.getElementById('inputHairColor').value,
+        oogkleur: document.getElementById('inputEyeColor').value,
+        grootte: document.getElementById('inputHeight').value,
+        gewicht: document.getElementById('inputWeight').value,
+        wachtwoord: document.getElementById('inputPassword1').value,
+        lovecoins: 3
 
-            const request = new Request(url, {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: new Headers({
-                    'Content-Type': 'application/json'
-                })
-            });
+    };
 
-            fetch(request)
-                .then(function (resp) {
-                    return resp.json();
-                })
-                .then(function (data) {
-                    console.log(data);
-                    location.replace('login.html')
-
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
+    alert(JSON.stringify(data))
 
 
-        } else{
-
-        }
-
-
+    const request = new Request(url, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
     });
+
+    fetch(request)
+        .then(function (resp) {
+            return resp.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            location.replace('login.html')
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }});
